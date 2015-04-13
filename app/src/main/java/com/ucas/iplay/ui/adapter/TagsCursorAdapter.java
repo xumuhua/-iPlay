6ea4fc46631809d589fa3ec4cf7936d74122e680
xumuhua <123138599@qq.com>
2015-04-13 22:39:42 +0800

    dsfwedfwer

---
+++ app/src/main/java/com/ucas/iplay/ui/adapter/TagsCursorAdapter.java
@@ -0,0 +1,79 @@
+package com.ucas.iplay.ui.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.ucas.iplay.core.db.TagsDataHelper;
+import com.ucas.iplay.core.model.TagModel;
+import com.ucas.iplay.ui.view.TagView;
+import com.ucas.iplay.ui.view.TagView.OnInterestedClickListener;
+import com.ucas.iplay.util.HttpUtil;
+import com.ucas.iplay.util.SPUtil;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+/**
+ * Created by ivanchou on 4/11/15.
+ */
+public class TagsCursorAdapter extends CursorAdapter implements OnInterestedClickListener {
+
+    private Context mContext;
+    private TagsDataHelper mTagsDataHelper;
+
+    public TagsCursorAdapter(Context context) {
+        super(context, null, false);
+        mContext = context;
+        mTagsDataHelper = new TagsDataHelper(context);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        getCursor().moveToPosition(position);
+        return TagModel.fromCursor(mContext, getCursor());
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return new TagView(context);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TagView tagView = (TagView) view;
+        TagModel tagModel = TagModel.fromCursor(context, cursor);
+        tagView.parse(tagModel);
+        tagView.setOnInterestedClickListener(this);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+
+    /**
+     * 订阅 Tag 的回调事件
+     * @param tagId
+     * @param interested
+     */
+    @Override
+    public void onClick(int tagId, int interested) {
+        mTagsDataHelper.updateInterested(tagId, interested);
+        long interestedTags = Long.valueOf(SPUtil.getSPUtil(mContext).get(SPUtil.INTERESTED_TAGS));
+        HttpUtil.changeInterestedTags(mContext, interestedTags, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ucas/iplay/ui/view/EventView.java
@@ -0,0 +1,84 @@
+package com.ucas.iplay.ui.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ucas.iplay.R;
+import com.ucas.iplay.core.model.EventModel;
+import com.ucas.iplay.core.model.UserModel;
+import com.ucas.iplay.ui.UserActivity;
+import com.ucas.iplay.util.StringUtil;
+
+/**
+ * Created by ivanchou on 4/5/15.
+ */
+public class EventView extends RelativeLayout implements View.OnClickListener{
+    private Context mContext;
+    public EventView(Context context) {
+        this(context, null);
+        mContext = context;
+    }
+
+    public EventView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EventView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private View mJointed;// 采用颜色标记用户是否已经参加某活动
+    private TextView mTitleTv;
+    private TextView mTimeScopeTv;
+    private ImageView mAvatorIv;
+    private TextView mTagNameTv;
+
+    private long mEventId;
+    private UserModel mUser;
+
+    private void init() {
+        inflate(getContext(), R.layout.event_lv_item, this);
+        mJointed = findViewById(R.id.v_jointed);
+        mTitleTv = (TextView) findViewById(R.id.tv_title);
+        mTimeScopeTv = (TextView) findViewById(R.id.tv_time_scope);
+        mAvatorIv = (ImageView) findViewById(R.id.iv_user_avatar);
+        mTagNameTv = (TextView) findViewById(R.id.tv_tag);
+        mAvatorIv.setOnClickListener(this);
+    }
+
+    public void parse(EventModel event) {
+        mEventId = event.eventId;
+        mTitleTv.setText(event.title);
+        String timeScope = StringUtil.parseLongTimeToString(event.startAt) + " ~ " + StringUtil.parseLongTimeToString(event.endAt);
+        mTimeScopeTv.setText(timeScope);
+//        mAvatorIv.setImageResource(event.author.avatar);
+//        mTagNameTv.setText(event.tags);
+
+    }
+
+    public long getEventId() {
+        return mEventId;
+    }
+
+    private void setJointed(View v) {
+        v.setBackgroundResource(android.R.color.holo_blue_bright);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_user_avatar:
+                Intent intent = new Intent(mContext, UserActivity.class);
+                mContext.startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
